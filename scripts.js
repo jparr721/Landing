a7e856da9582f28e08ada73e0ebe4949944c3574
Jarred Parr <jparr721@gmail.com>
2017-07-18 14:04:16 -0400

    Nearly done. Also added utility library

--- scripts.js
+++ scripts.js
@@ -11,11 +11,32 @@ $(document).ready(function(){
 
     $('html, body').animate({
       scrollTop: $(hash).offset().top
-    }, 1000, function(){
+    }, 800, function(){
 
 
       window.location.hash = hash;
       });
     }
+  });
+
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 600) {
+        $(this).addClass("slide");
+      }
+    });
+  });
+  $(window).scroll(function() {
+    $(".iconanim").each(function() {
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+      if (pos < winTop + 630) {
+        $(this).addClass("iconAnimate");
+      }
+    });
   });
 })
