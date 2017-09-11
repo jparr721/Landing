cc812f57c0747f9638825fd6c840beb5e7acc258
Kyle Flynn <kyle.flynn5798@yahoo.com>
2017-09-11 16:30:34 -0400

    Updates that I should've uploaded sooner.

---
+++ landing/src/components/portfolio.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default Portfolio;
--- landing/src/components/routes.js
+++ landing/src/components/routes.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import App from '../App';
 import Home from './home';
+import Portfolio from './portfolio';
 import HireUs from './hireUs';
 
 const Routes = () => {
   return (
       <routes>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/hire-us' component={HireUs} />
-        </Switch>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/portfolio' component={Portfolio}/>
+            <Route path='/hire-us' component={HireUs} />
+          </Switch>
       </routes>
   )
 };
